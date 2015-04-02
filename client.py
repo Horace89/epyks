194831a705d9e70daf07477920f933fe68227bc4
edikwow <decimal.counter@gmail.com>
2015-04-03 00:19:05 +0300

    Reworked SoundIO with pure functions

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ import threading
 # Shit works like this:
 #
 # -> -> ClientA.Record(CHUNK_SIZE) -> CHUNK -> INPUT_QUEUE_A.PUT(CHUNK) -> ClientA.Send(INPUT_QUEUE_A.get()) ->
-#       -------------------------------------- MEANWHILE ON CLIENT B ---------------------------------------  |
+# -------------------------------------- MEANWHILE ON CLIENT B ---------------------------------------  |
 # < -<- ClientB.Play(CHUNK)  <-  OUTPUT_QUEUE_B.PUT(CHUNK)  <- CHUNK <- ClientB.RecV(CHUNK_SIZE)<- <- <- <- <-
 #
 # So we need two Queues on each server: output and input
@@ -34,79 +34,34 @@ STOP_SOUND_IO = threading.Event()
 # it to the ClientB
 CHUNK = 1024
 INPUT_PARAMS = {
-    'frames_per_buffer': CHUNK,    # Check if this is best way
-    'format': pyaudio.paInt16,    # Not sure about that either
-    'rate': 10000,                # Lowering rate for the very begining
-    'channels': 1,                # My mic has one channel, so...
+    'frames_per_buffer': CHUNK,  # Check if this is best way
+    'format': pyaudio.paInt16,  # Not sure about that either
+    'rate': 10000,  # Lowering rate for the very begining
+    'channels': 1,  # My mic has one channel, so...
     'input': True,
 }
 OUTPUT_PARAMS = {  # Pretty much same as input
-    'frames_per_buffer': CHUNK,
-    'format': pyaudio.paInt16,
-    'rate': 10000,
-    'channels': 1,
-    'output': True,
+                   'frames_per_buffer': CHUNK,
+                   'format': pyaudio.paInt16,
+                   'rate': 10000,
+                   'channels': 1,
+                   'output': True,
 }
 
+PA = pyaudio.PyAudio()
 
-class SoundIO(object):
-    CURRENT_READ_CHUNK = None
+def player():
+    stream = PA.open(**OUTPUT_PARAMS)
+    while not STOP_SOUND_IO.is_set():
+        block = OUTPUT_QUEUE.get()
+        stream.write(block)
+    stream.stop_stream()
+    stream.close()
 
-    def __new__(cls, *args, **kwargs):
-        """
-        We want our class to be a singletone
-        """
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(SoundIO, cls).__new__(cls, *args, **kwargs)
-        return cls.instance
-
-    def __init__(self):
-        self.audio_streams = {
-            'input': PyAudio.open(**stream_defaults['input']),
-            'output': PyAudio.open(**stream_defaults['output'])
-        }
-        self.out_write = self.audio_streams['output'].write
-        self.in_read = self.audio_streams['input'].read
-        for stream in self.audio_streams.values():
-            stream.start_stream()
-
-    def play(self, chunk):
-        self.out_write(chunk)
-
-    def record(self, chunk_size):
-        self.CURRENT_READ_CHUNK = self.in_read(chunk_size)
-
-    def stop(self):
-        for stream in self.audio_streams.values():
-            stream.stop_stream()
-            stream.close()
-
-    def __del__(self):
-        self.stop()
-
-
-class OuterPlayer(threading.Thread):
-    def __init__(self, *args, **kwargs):
-        super(OuterPlayer, self).__init__(*args, **kwargs)
-
-    def run(self):
-        p = pyaudio.PyAudio()  # TODO: should this be global?
-        stream = p.open(**OUTPUT_PARAMS)
-        while not STOP_SOUND_IO.is_set():
-            block = OUTPUT_QUEUE.get()
-            stream.write(block)
-        stream.stop_stream()
-        stream.close()
-
-class InnerRecorder(threading.Thread):
-    def __init__(self, *args, **kwargs):
-        super(InnerRecorder, self).__init__(*args, **kwargs)
-
-    def run(self):
-        p = pyaudio.PyAudio()
-        stream = p.open(**INPUT_PARAMS)
-        while not STOP_SOUND_IO.is_set():
-            block = stream.read(CHUNK)
-            INPUT_QUEUE.put(block)
-        stream.stop_stream()
-        stream.close()
+def recorder():
+    stream = PA.open(**INPUT_PARAMS)
+    while not STOP_SOUND_IO.is_set():
+        block = stream.read(CHUNK)
+        INPUT_QUEUE.put(block)
+    stream.stop_stream()
+    stream.close()
