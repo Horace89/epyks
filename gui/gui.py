89fa1d1aab97da76bd699a338917a37d666bd258
edikwow <decimal.counter@gmail.com>
2015-04-10 22:45:30 +0300

    Implementing gui

---
+++ gui/gui.py
@@ -0,0 +1,61 @@
+import os
+import npyscreen
+from networking import base
+
+#
+# #
+# class Asdf(npyscreen.Form):
+#     def create(self):
+#         super(myEmployeeForm, self).create()
+#         self.name = self.add(npyscreen.TitleText, name='name')
+#         self.department = self.add(npyscreen.TitleSelectOne, name='department',
+#                                    max_height=3,
+#                                    values=['black mesa', 'nova prospect', 'city 17'],
+#                                    scroll_exit=True)
+#         self.date = self.add(npyscreen.TitleDateCombo, name='date joined')
+#
+#     def afterEditing(self):
+#         self.parentApp.setNextForm(None)
+
+
+class MainForm(npyscreen.Form):
+    OK_BUTTON_TEXT = "EXIT"  # well...
+
+
+    def create(self):
+        super(MainForm, self).create()
+        self.name = self.add(npyscreen.TitleText, name="username", value="username")
+        self.parentApp.setNextForm('username_form')
+
+
+    def afterEditing(self):
+        self.parentApp.setNextForm(None)
+
+
+class IntroduceYourself(npyscreen.Popup):
+    def create(self):
+        self.greeting = self.add(npyscreen.TitleText, name="Username", value=os.getenv('USER'))
+        self.lo_addr = self.add(npyscreen.TitleFixedText, name="Your address", value=base.get_local_addr())
+        npyscreen.notify_confirm(message="yo")
+
+    def afterEditing(self):
+        self.parentApp.mainform.name.value = self.greeting.value
+        self.parentApp.setNextForm('MAIN')
+
+
+class Application(npyscreen.NPSAppManaged):
+    def onStart(self):
+        self.addForm('MAIN', MainForm, name="Main form")
+        self.addForm('username_form', IntroduceYourself, name="Choose username")
+        self.mainform = self.getForm('MAIN')  # Dont know how to bind it other way
+
+
+# def my_function(*args):
+#     F = Asdf(name='New employee')
+#     F.edit()
+#     return 'Recotd for: ' + F.name.value
+
+def run():
+    App = Application().run()
+
+initialize = run
--- main.py
+++ main.py
@@ -1,32 +1,58 @@
+import sys
 import threading
-from _socket import error as socket_error
 
-import console
+from console import console
+from gui import gui
+
+from _socket import error as socket_error
 from networking.server import UDPServer
 from sound.io import player, recorder
 
+# All this module should do is to define whether GUI version
+# should be used or vannila-console
 
-def console_listner(HOST, PORT):
-    server = UDPServer((HOST, PORT))
-    srv_thread = threading.Thread(target=server.serve_forever, name='ServerThread')
-    playback_thread = threading.Thread(target=player, name='PlaybackThread')
-    recorder_thread = threading.Thread(target=recorder, name='RecordSoundThread')
-    # srv_thread.setDaemon(True)
-    srv_thread.start()
-    playback_thread.start()
-    recorder_thread.start()
-    console.commander(server_instance=server)
-    server.shutdown()
+HOST, PORT = 'localhost', 8888
+#
+# def console_listner(HOST, PORT):
+#     server = UDPServer((HOST, PORT))
+#     srv_thread = threading.Thread(target=server.serve_forever, name='ServerThread', daemon=True)
+#     playback_thread = threading.Thread(target=player, name='PlaybackThread')
+#     recorder_thread = threading.Thread(target=recorder, name='RecordSoundThread')
+#     # srv_thread.setDaemon(True)
+#     srv_thread.start()
+#     playback_thread.start()
+#     recorder_thread.start()
+#     console.commander(server_instance=server)
+#     server.shutdown()
+
+
+def main():
+    mode = '-console'  # default mode
+    modes = ('-console', '-gui')
+    try:
+        mode = sys.argv[1]
+    except IndexError:
+        # Using default
+        pass
+    if mode == '-console':
+        console.initialize()
+    elif mode == '-gui':
+        gui.initialize()
+    else:
+        print 'Unknown mode {}, only those {} acceptable'.format(mode, modes)
 
 
 if __name__ == '__main__':
-    console.greetings()
-    HOST, PORT = 'localhost', 8888
-    while True:
-        try:
-            console_listner(HOST, PORT)
-            break
-        except socket_error as ex:
-            if ex.errno == 98:
-                print "Port {} already in use, trying out next".format(PORT)
-                PORT += 1
+    main()
+#
+# if __name__ == '__main__a':
+#     console.greetings()
+#     HOST, PORT = 'localhost', 8888
+#     while True:
+#         try:
+#             console_listner(HOST, PORT)
+#             break
+#         except socket_error as ex:
+#             if ex.errno == 98:
+#                 print "Port {} already in use, trying out next".format(PORT)
+#                 PORT += 1
