ba8bacdf1734cfab6b959589a4c4de8fa891b964
edikwow <decimal.counter@gmail.com>
2015-04-15 23:51:12 +0300

    Tk gui implementations

--- gui/tkgui.py
+++ gui/tkgui.py
@@ -3,7 +3,17 @@ from Tkinter import Tk, BOTH
 from ttk import Frame, Button, Style, Entry
 import tkMessageBox
 
+ACCEPTABLE_CHARS = "1234567890:."
 
+
+def is_char_acceptable(char):
+    global ACCEPTABLE_CHARS
+    if char in ACCEPTABLE_CHARS:
+        return True
+    return False
+
+
+# noinspection PyAttributeOutsideInit
 class Example(Frame):
     """
     If method is handling some GUI shit, its written in camelCase style
@@ -19,18 +29,25 @@ class Example(Frame):
         self.__centerWindow()
         self.parent.title("epyks!")
         self.pack(fill=BOTH, expand=1)
-        quitButton = Button(self, text="Call",  command=self.onCallClick)
-        quitButton.place(x=50, y=50)
-        #addressTextBox = Entry(self,) # command=self.onTextBoxChange)
-        #addressTextBox.place(x=200, y=50)
-        okayCommand = self.register(self.isOkay)
-        self.w = Entry(self, validate='key', validatecommand=(okayCommand, '%P'))
-        self.w.place(x=100, y=50)
+        #
+        #   Call button
+        #
+        self.buttonCall = Button(self, text="Call",  command=self.onCallClick)
+        self.buttonCall.place(x=50, y=50)
+        #
+        #   Addr textbox
+        #
+        addr_validate = (self.parent.register(self.addrValidation), '%S',)
+        self.entryAddress = Entry(self, validate='key', validatecommand=addr_validate, background="black")
+        self.entryAddress.place(x=100, y=50)
+
+    def addrValidation(self, char):
+        print "addrValidation: %s" % (char)
+        return is_char_acceptable(char)
 
 
     def isOkay(self, what):
-        print what
-        return True
+        return is_char_acceptable(what)
 
     def onTextBoxChange(self, *args, **kwargs):
         print 'lol ' + str(args) + str(kwargs)
